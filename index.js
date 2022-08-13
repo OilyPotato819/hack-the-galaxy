7067681638d9185f1adc3a0fce53dbbc28d8fd83
OilyPotato819 <o.schneider@share.epsb.ca>
2022-08-12 22:51:54 -0600

    a

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ allWords.forEach((word) => {
 });
 
 const signals = [
-   'pphiobatneterey',
    //    'papsfrutesutternet',
    //    'epalwrotavey',
    //    'licagamchitetraison',
@@ -58,7 +57,6 @@ const signals = [
 ];
 
 const validWords = {
-   pphiobatneterey: new Map(),
    papsfrutesutternet: new Map(),
    epalwrotavey: new Map(),
    licagamchitetraison: new Map(),
@@ -89,47 +87,5 @@ signals.forEach((signal) => {
             return;
          }
       }
-
-      validWords[signal].set(firstWord, []);
-      console.log(signal, lettersLeft2, firstWord);
-
-      goodWords.forEach((secondWord) => {
-         if (secondWord === firstWord) return;
-
-         let lettersLeft3 = lettersLeft2;
-
-         for (let letterNum = 0; letterNum < secondWord.length; letterNum++) {
-            const letter = secondWord.charAt(letterNum);
-            const index = lettersLeft2.indexOf(letter);
-
-            if (index > -1) {
-               lettersLeft2 = lettersLeft2.slice(index + 1);
-               lettersLeft3 = lettersLeft3.replace(letter, '');
-            } else {
-               return;
-            }
-         }
-
-         goodWords.forEach((thirdWord) => {
-            if (thirdWord === firstWord || thirdWord === secondWord) return;
-
-            for (let letterNum = 0; letterNum < thirdWord.length; letterNum++) {
-               const letter = thirdWord.charAt(letterNum);
-
-               const index = lettersLeft3.indexOf(letter);
-               // console.log(index, letter, lettersLeft3);
-
-               if (index > -1) {
-                  lettersLeft3 = lettersLeft3.slice(index + 1);
-               } else {
-                  return;
-               }
-            }
-
-            validWords[signal].get(firstWord).push([secondWord, thirdWord]);
-         });
-      });
    });
 });
-
-console.log(validWords.pphiobatneterey);
